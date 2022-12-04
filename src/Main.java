a76419adf83b81f5815377dc8ef1e9d9097d6673
Brustik <minusminusov@gmail.com>
2022-12-04 05:07:32 +0300

    ДЗ Условные операторы 1 - Задание 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,21 @@ public class Main {
             System.out.println("Возраст совершеннолетия еще не наступил. Подожди немного!");
         }
 
+        //Задание 2
+        int age2 = 5;
+        if (age2 >= 24) {
+            System.out.println("Окончил ВУЗ и пора искать работу");
+        } else
+        if (age2 >= 18) {
+            System.out.println("Уже закончил школу и можно отправляться в ВУЗ");
+        } else
+        if (age2 >= 7) {
+            System.out.println("Ребенок ходит в школу");
+                        } else {
+            System.out.println("Еще маленький, ходит в детсад");
+        }
+
+
 
 
 

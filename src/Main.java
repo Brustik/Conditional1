64b1a3724c704b59002b473768a319abbbc57c4f
Brustik <minusminusov@gmail.com>
2022-12-04 04:51:57 +0300

    ДЗ Условные операторы 1 - Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Позравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил. Подожди немного!");
+        }
+
+
+
+
+
 
            }
 }
